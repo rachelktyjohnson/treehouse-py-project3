b2ed62b7b8e3f7b612ec840cc186aa8b39b24780
Rachel Johnson <rachelktyjohnson@gmail.com>
2021-06-16 16:24:47 +1000

    Flesh out Phrase class

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 # Import your Game class
 
-# Create your Dunder Main statement.
-
-# Inside Dunder Main:
-## Create an instance of your Game class
-## Start your game by calling the instance method that starts the game loop
+if __name__ == "__main__":
+    # Create an instance of your Game class
+    # Start your game by calling the instance method that starts the game loop
+    pass
--- phrasehunter/phrase.py
+++ phrasehunter/phrase.py
@@ -1,1 +1,34 @@
 # Create your Phrase class logic here.
+class Phrase:
+    def __init__(self, in_phrase):
+        self.phrase = in_phrase.lower()
+        self.phrase_list = []
+        self.phrase_display = []
+        for char in self.phrase:
+            self.phrase_list.append(char)
+            if char == " ":
+                self.phrase_display.append(" ")
+            else:
+                self.phrase_display.append("_")
+
+    def display(self):
+        #  prints out the phrase to the console with _ or letter
+        print(" ".join(self.phrase_display))
+
+    def check_letter(self, in_letter):
+        #  checks to see if letter selected matches a letter in the phrase
+        if in_letter in self.phrase_list:
+            for index, char in enumerate(self.phrase_list):
+                if char == in_letter:
+                    self.phrase_display[index] = in_letter
+            return True
+        else:
+            return False
+
+    def check_complete(self):
+        #  checks to see if the whole phrase has been guessed
+        return "_" not in self.phrase_display
+
+
+phrase = Phrase("hello wo")
+phrase.display()
